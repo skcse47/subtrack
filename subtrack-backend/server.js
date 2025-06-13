58d1268323d8061fdd6f8765d4a7396dcbaa6778
skcse47 <skcse47@gmail.com>
2025-06-13 23:51:32 +0530

    feat: add cookie-parser and express-rate-limit dependencies; create docker-compose for Redis and MongoDB services

--- subtrack-backend/server.js
+++ subtrack-backend/server.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const connectDB = require('./config/db'); 
 const {getuser} = require('./practice')
 const cookieParser = require('cookie-parser');
-const limiter = require('./utils/rateLimit');
 
 dotenv.config();
 const app = express();
